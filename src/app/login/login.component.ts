3689bb005473401b2de532ff83e6ec3a500a6946
mohamed <mohamed-ali.rais-prestataire@bforbank.com>
2021-02-08 20:08:01 +0100

    feat(monster-front) implement rmember me

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -25,6 +25,7 @@ export class LoginComponent implements OnInit {
   displayLoginErrorMsg: boolean = false;
   isUserDesabledErrorMsg: boolean = false;
   returnUrl: string;
+  private rememberMe: boolean = false;
 
   constructor(private individuApiService: IndividuApiService, private individuService: IndividuService, private router: Router, private route: ActivatedRoute) {
   }
@@ -37,6 +38,7 @@ export class LoginComponent implements OnInit {
   ngOnInit() {
     this.user = new User();
     this.returnUrl = this.route.snapshot.queryParams['returnUrl'] || '/';
+    this.verifyRememberMe();
   }
 
   login(): void {
@@ -51,6 +53,7 @@ export class LoginComponent implements OnInit {
           this.individuApiService.retrieveIndividu(this.user.login).subscribe(indiv => {
             this.individuService.connectedUserInfo = indiv.individu;
             this.individuService.connectedUserRole = indiv.role;
+            this.updateRememberMe();
             this.router.navigateByUrl(this.returnUrl);
           });
         } else {
@@ -61,6 +64,25 @@ export class LoginComponent implements OnInit {
       }
     });
   }
+
+  updateRememberMe(){
+    localStorage.removeItem('login');
+    localStorage.removeItem('RememberMe');
+    if(this.rememberMe){
+      localStorage.setItem('login', this.user.login);
+      localStorage.setItem('RememberMe', JSON.stringify(this.rememberMe));
+    }
+  }
+
+  verifyRememberMe(){
+    if(JSON.parse(localStorage.getItem('RememberMe')) !== null)
+    {
+      this.user.login = localStorage.getItem('login');
+      this.rememberMe = JSON.parse(localStorage.getItem('RememberMe'));
+    }
+  }
+
+
 
   displayErrorMessage(isLogginError: boolean) {
     this.displayLoginErrorMsg = isLogginError;
